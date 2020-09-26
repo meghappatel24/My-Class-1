b08bddc6aa041b63a7e912f273718e6612f84490
meghappatel24 <71676491+meghappatel24@users.noreply.github.com>
2020-09-26 16:28:10 -0400

    Add files via upload

---
+++ myclass1.java
@@ -0,0 +1,10 @@
+package Assignment;
+
+public class myclass1 {
+	public static void main(String []args) {
+		 System.out.println("Megha Patel");
+		 System.out.println("ID# 0620488");
+		 System.out.println("Email - mpatel31@nyit.edu");
+
+}
+}
